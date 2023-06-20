4752d05148316063ce29180669068d554cde37c1
csh <tmdgjs7495@naver.com>
2023-06-20 21:27:37 +0900

    [test] : SpringBatch 테스트

---
+++ src/test/java/com/springbatch/practice/SpringBatchTestConfig.java
@@ -0,0 +1,13 @@
+package com.springbatch.practice;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+@EnableAutoConfiguration // 무슨 애노테이션?
+public class SpringBatchTestConfig {
+
+
+}
---
+++ src/test/java/com/springbatch/practice/job/helloworld/HelloWorldJobConfigTest.java
@@ -0,0 +1,37 @@
+package com.springbatch.practice.job.helloworld;
+
+import com.springbatch.practice.SpringBatchTestConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// 하나의 Job 은 하나의 Test 가능.
+
+@RunWith(SpringRunner.class)
+@SpringBatchTest
+@SpringBootTest(classes = {SpringBatchTestConfig.class, HelloWorldJobConfig.class})
+class HelloWorldJobConfigTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Test
+    public void success() throws Exception {
+        // when
+        JobExecution execution = jobLauncherTestUtils.launchJob();
+
+        // then
+        Assertions.assertEquals(execution.getExitStatus(), ExitStatus.COMPLETED);
+    }
+
+
+}
